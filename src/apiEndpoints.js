80eef45ca50da2e6919557cd97d6144272a61bc2
Valerie Lui <vallq@users.noreply.github.com>
2020-02-13 11:17:29 +0800

    Added API endpoints to separate file

---
+++ src/apiEndpoints.js
@@ -0,0 +1,9 @@
+const apiEndpoints = {
+  "0": "GET /",
+  "1": "GET /companies",
+  "2": "GET /companies/:id",
+  "3": "POST /companies/:id/reviews",
+  "4": "GET /user"
+};
+
+module.exports = apiEndpoints;
